ca5734cdaa4b83e3d539a7ec47a0e342abcdfee9
WilliamCai18 <WilliamCai_Apple@icloud.com>
2019-03-12 16:53:56 +0800

    Add MergeSort

---
+++ src/com/sort/HeapSort.java
@@ -0,0 +1,19 @@
+package com.sort;
+
+public class HeapSort {
+    private static void adjustHeap(int[] a) {
+        int child;
+    }
+
+    private static void heapSort(int[] a) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] a = {8, -1, 6, 4, 5, 2, 9, 7, 1, 0, 3};
+        heapSort(a);
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+    }
+}
--- src/com/sort/MergeSort.java
+++ src/com/sort/MergeSort.java
@@ -1,11 +1,41 @@
 package com.sort;
 
 public class MergeSort {
-    private static void mergeSort() {
+    private static void merge(int[] a, int low, int mid, int high) {
+        int[] tmp = new int[high - low + 1];
+        int i = low;
+        int j = mid + 1;
+        int k = 0;
+        while (i <= mid && j <= high) {
+            if (a[i] < a[j]) {
+                tmp[k++] = a[i++];
+            } else {
+                tmp[k++] = a[j++];
+            }
+        }
+        while (i <= mid) {
+            tmp[k++] = a[i++];
+        }
+        while (j <= high) {
+            tmp[k++] = a[j++];
+        }
+        if (tmp.length >= 0) System.arraycopy(tmp, 0, a, low, tmp.length);
+    }
 
+    private static void mergeSort(int[] a, int low, int high) {
+        int mid = (low + high) / 2;
+        if (low < high) {
+            mergeSort(a, low, mid);
+            mergeSort(a, mid + 1, high);
+            merge(a, low, mid, high);
+        }
     }
 
     public static void main(String[] args) {
-
+        int[] a = {4, 2, 1, 6, 9, 7, -1, 8, 5, 3};
+        mergeSort(a, 0, a.length - 1);
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
     }
 }
